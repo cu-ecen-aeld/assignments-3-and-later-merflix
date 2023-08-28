4e9ecbfbe0a14411277b761a0885c1032064f7d7
merflix <merflix@gmail.com>
2023-08-28 10:33:03 +0200

    attempt-2

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/bash
-# p2
 
-filesdir=$1
+filesdir=$1 
 searchstr=$2
 
-if [ $# != 2 ]; then
-    echo "Not all parameters were given!"
+if [ -z $filesdir ] || [ -z $searchstr ]; then
+    echo "Missing parameters"
     exit 1
 fi
 
-if [ ! -d "$filesdir" ]; then
-    echo "Not a valid path: $filesdir"
+if [ ! -d $filesdir ]; then
+    echo "Directory is invalid"
     exit 1
 fi
 
-nof_expressions=$(grep -Ir "$searchstr" "$filesdir" | wc -l)
-nof_files=$(grep -Ir "$searchstr" "$filesdir" | cut -d ':' -f 1 | uniq -c | wc -l)
 
-echo "The number of files are $nof_files and the number of matching lines are $nof_expressions"
+X=$(find $filesdir -type f | wc -l)
+Y=$(grep -r $searchstr $filesdir | wc -l)
+
+echo "The number of files are $X and the number of matching lines are $Y"
+
 
