236bc057160c9df9ee266a7e202e6db5e523600c
merflix <merflix@gmail.com>
2023-09-03 10:10:03 +0200

    attempt-1

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -2,11 +2,13 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <errno.h>
 
 // Optional: use these functions to add debug or error prints to your application
 #define DEBUG_LOG(msg,...)
 //#define DEBUG_LOG(msg,...) printf("threading: " msg "\n" , ##__VA_ARGS__)
 #define ERROR_LOG(msg,...) printf("threading ERROR: " msg "\n" , ##__VA_ARGS__)
+
 
 void* threadfunc(void* thread_param)
 {
@@ -14,6 +16,18 @@ void* threadfunc(void* thread_param)
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    
+    sleep( thread_func_args->wait_to_obtain_ms );
+    pthread_mutex_lock (thread_func_args->mutex); 
+
+    thread_func_args->thread_complete_success = true;
+    printf("set thread_complete_success = true\n");
+
+    sleep(thread_func_args->wait_to_release_ms );
+    pthread_mutex_unlock (thread_func_args->mutex); 
+
+
     return thread_param;
 }
 
@@ -28,6 +42,28 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+    struct thread_data *ptr;
+    ptr = (struct thread_data *)malloc(sizeof(struct thread_data)); // dynamic memory allocation
+    if( ptr == NULL ) { 
+        printf("memory allocation to thread_data failed\n");
+        return false;
+    }
+
+    errno = pthread_mutex_init(mutex, NULL);
+    
+    ptr->wait_to_obtain_ms  = wait_to_obtain_ms;
+    ptr->wait_to_release_ms = wait_to_release_ms;
+    ptr->mutex = mutex;
+
+    pthread_create(thread, NULL, threadfunc, (void *)ptr);
+    ptr->thread_complete_success = true;
+    printf("THREAD TERMINATED WITH SUCCESS %d\n",ptr->thread_complete_success );
+    ptr->thread_complete_success = true;
+
+    pthread_join((*thread), NULL);
+
+    free(ptr);
+
+    return true;
 }
 
